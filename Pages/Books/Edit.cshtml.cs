1488b541d6d0cfd80729a827d7308eae65ed7dda
mihairujoiu956-ux <mihairujoiu956@gmail.com>
2025-11-09 14:08:00 +0200

    terminat ok

--- Pages/Books/Edit.cshtml.cs
+++ Pages/Books/Edit.cshtml.cs
@@ -78,7 +78,7 @@ namespace Rujoiu_Mihai_Lab2.Pages.Books
             if (await TryUpdateModelAsync<Book>(
                 bookToUpdate,
                 "Book",
-                i => i.Title, i => i.Author,
+                i => i.Title, i => i.AuthorID,
                 i => i.Price, i => i.PublishingDate, i => i.PublisherID))
             {
                 UpdateBookCategories(_context, selectedCategories, bookToUpdate);
